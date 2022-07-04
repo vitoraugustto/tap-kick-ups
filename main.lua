4e9d4850fb0d4b8f9249839ace99de3fb19bcdc5
Vitor <vitoraugusto002@hotmail.com>
2022-07-04 00:59:31 -0300

    add rotation when ball collides with walls

--- main.lua
+++ main.lua
@@ -17,9 +17,15 @@ local RIGHT = display.contentCenterX + display.actualContentWidth * 0.5
 local BOTTOM = display.contentCenterY + display.actualContentHeight * 0.5
 
 local leftWall  = display.newLine ( LEFT, TOP, LEFT, BOTTOM )
+leftWall.name = 'leftWall'
 local rightWall = display.newLine ( RIGHT, TOP, RIGHT, BOTTOM )
+rightWall.name = 'rightWall'
 local ceiling = display.newLine ( LEFT, TOP, RIGHT, TOP )
+ceiling.name = 'ceiling'
 local bottom = display.newLine ( LEFT, BOTTOM, RIGHT, BOTTOM )	
+bottom.name = 'bottom'
+
+local walls = { leftWall, rightWall, ceiling, bottom }
 
 physics.addBody( leftWall, 'static', { density=1.0, friction=0.3, bounce=0.2 } )
 physics.addBody( rightWall, 'static', { density=1.0, friction=0.3, bounce=0.2 } )
@@ -32,12 +38,13 @@ background.x = display.contentCenterX
 background.y = display.contentCenterY
 
 local platform = display.newImageRect( "src/assets/images/platform.png", 360, 60 )
-platform.name = "Platform"
+platform.name = "platform"
 platform.x = display.contentCenterX
 platform.y = display.contentHeight + 100
 physics.addBody( platform, "static" )
 
 local ball = display.newImageRect( "src/assets/images/ball.png", 112, 112 )
+ball.name = 'ball'
 ball.x = display.contentCenterX
 ball.y = display.contentCenterY
 physics.addBody( ball, "dynamic", { radius=50, bounce=0.7 } )
@@ -49,40 +56,76 @@ local kickSounds = {
 }
 
 local tapCount = 0
-local tapText = display.newText( tapCount, display.contentCenterX, 20, native.systemFont, 40 )
+local tapText = display.newText( tapCount, display.contentCenterX, 20, native.systemFont, 50 )
 tapText:setFillColor( 0, 0, 0 )
+
+local record = 0
+local recordText = display.newText( 'Recorde:' .. ' ' .. record, display.contentCenterX, -50, native.systemFont, 30 )
+recordText:setFillColor( 0, 0, 0 )
 
 local maxKickSound = 8
 local numberOfKicks = 0
 
+local rotation = 0
+
 local function pushBall( event )
+  local impulseDirection = math.random( -10, 10 ) / 10
   if ( event.phase == 'began' ) then
+
     numberOfKicks = 0
     audio.setVolume( 1, { channel=kickSounds } )
     audio.play( kickSounds[math.random( 1, 3 )] )
-    ball:applyLinearImpulse( math.random( -10, 10 ) / 10, -4, ball.x, ball.y )
+    ball:applyLinearImpulse( impulseDirection , -4, ball.x, ball.y )
 
     tapCount = tapCount + 1
     tapText.text = tapCount
+
+    if ( tapCount > record ) then
+      record = tapCount
+      recordText.text = 'Recorde:' .. ' ' .. record
+    end
   end
 end
 
-local function onCollision( self, event )
-  if ( event.other.name == 'Platform' ) then
+local function onPlatformCollision( self, event )
+  if ( event.other.name == 'platform' ) then
     if ( event.phase == "began" ) then
       numberOfKicks = numberOfKicks + 1
       if ( numberOfKicks < maxKickSound ) then
         audio.setVolume( 0.2, { channel=kickSounds } )
         audio.play( kickSounds[math.random( 1, 3 )] )
       end
-      if ( ball.y <= 200 ) then audio.play( kickSounds[math.random( 1, 3 )] ) end
+
+      if ( numberOfKicks == maxKickSound ) then
+        ball.x = 100
+      end
       tapCount = 0
       tapText.text = tapCount
     end
   end
 end
-  
-ball.collision = onCollision
+
+local function onWallCollision( self, event )
+  if ( event.other.name == 'ball' ) then
+    if ( event.phase == 'began' ) then
+      print(ball.isAwake)
+
+      if ( event.target.name == 'leftWall' ) then
+        transition.to( ball, { rotation=360, time=10000 } )
+      elseif ( event.target.name == 'rightWall' ) then
+        transition.to( ball, { rotation=-360, time=10000 } )
+      end
+    end
+  end
+end
+
+for i = 1, #walls do
+  walls[i].collision = onWallCollision  
+  walls[i]:addEventListener( "collision" )
+end
+
+ball.collision = onPlatformCollision
 ball:addEventListener( "collision" )
 ball:addEventListener( "touch", pushBall )
+
 
